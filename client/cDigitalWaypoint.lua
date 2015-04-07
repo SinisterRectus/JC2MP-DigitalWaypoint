782147c45dae1f31d8c8d0ba05d2a2ac08181842
SinisterRectus <SinisterRectus@users.noreply.github.com>
2015-04-07 13:12:29 -0400

    Update cDigitalWaypoint.lua

--- client/cDigitalWaypoint.lua
+++ client/cDigitalWaypoint.lua
@@ -2,8 +2,8 @@
 
 class 'DigitalWaypoint'
 
--- Set waypoint using: /sw # #
--- #k is accepted (Ex: 25.4k returns 25400)
+-- Set waypoint using: /sw [number] [number]
+-- "k" format is accepted (Ex: 25.4k returns 25400)
 -- Clear waypoint using: /cw
 -- Get waypoint using: /gw
 -- Add custom waypoints in the self.custom table
@@ -14,7 +14,7 @@ function DigitalWaypoint:__init()
 	self.msg_color = Color(192, 192, 192) -- Local chat messages
 	
 	self.custom = {} -- Add custom waypoints here
-	self.custom["pia"] = {9610, 12760, "Panau Intl Airport"}
+	self.custom["pia"] = {9610, 12760, "Panau International Airport"}
 
 	Events:Subscribe("LocalPlayerChat", self, self.Control)
 	
@@ -22,7 +22,7 @@ end
 
 function DigitalWaypoint:Control(args) -- Subscribed to LocalPlayerChat
 
-	local cmd_string = tostring(args.text:split(" ")[1])
+	local cmd_string = args.text:split(" ")[1]
 	local x_string = tostring(args.text:split(" ")[2])
 	local y_string = tostring(args.text:split(" ")[3])
 	
@@ -41,20 +41,22 @@ function DigitalWaypoint:Control(args) -- Subscribed to LocalPlayerChat
 	elseif string.find(y_string, "k") and y_string ~= "k" and string.sub(y_string, string.find(y_string, "k") + 1) == "" and tonumber(y_string:split("k")[1]) then
 	-- Checks y input string for a "k" with nothing after it, but with a number before it
 		m = tonumber(y_string:split("k")[1]) * 1000
-	end	
-		
-	if cmd_string == "/sw" and n and m then
-		Waypoint:SetPosition(Vector3(n - 16384, 0, m - 16384))
-		Chat:Print("Waypoint set "..tostring(math.floor(Waypoint:GetDistance() + 0.5)).." m away at x = "..tostring(n).." m, y = "..tostring(m).. " m", self.msg_color)
-		return false
-	elseif cmd_string == "/sw" and self.custom[x_string] then
-		n = self.custom[x_string][1]
-		m = self.custom[x_string][2]
-		Waypoint:SetPosition(Vector3(n - 16384, 0, m - 16384))
-		Chat:Print("Waypoint set "..tostring(math.floor(Waypoint:GetDistance() + 0.5)).." m away at x = "..tostring(n).." m, y = "..tostring(m).." m for "..tostring(self.custom[x_string][3]), self.msg_color)
-		return false
-	elseif cmd_string == "/sw" and not (self.custom[x_string] or (n and m)) then
-		Chat:Print("Invalid waypoint", self.msg_color)
+	end
+	
+	print(#args.text:split(" "))
+	
+	if cmd_string == "/sw" then
+		if n and m and #args.text:split(" ") == 3 then
+			Waypoint:SetPosition(Vector3(n - 16384, 0, m - 16384))
+			Chat:Print("Waypoint set at x = "..tostring(n).." m, y = "..tostring(m).. " m", self.msg_color)
+		elseif self.custom[x_string] and #args.text:split(" ") == 2 then
+			n = self.custom[x_string][1]
+			m = self.custom[x_string][2]
+			Waypoint:SetPosition(Vector3(n - 16384, 0, m - 16384))
+			Chat:Print("Waypoint set at "..tostring(self.custom[x_string][3]), self.msg_color)
+		else
+			Chat:Print("Invalid waypoint", self.msg_color)
+		end
 		return false
 	end
 	
